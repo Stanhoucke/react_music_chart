079666dc0c852078ba4d93df7f7c9f4a68d95231
Stanhoucke <stanhoucke@gmail.com>
2021-03-02 20:43:50 +0000

    song previews change on click

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,6 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 
 const SongDetails = ({details, loaded}) => {
+
+    const audioRef = useRef();
+    const [source, setSource] = useState("");
+
+    useEffect(() => {
+        updateSong();
+    }, [details])
+
+    const updateSong = () => {
+        if(loaded) {
+            setSource(details.link[1].attributes.href);
+            if(audioRef.current){
+                audioRef.current.pause();
+                audioRef.current.load();
+            }
+        }
+    }
 
     if(!loaded) {
         return <p>Loading...</p>
@@ -9,7 +26,7 @@ const SongDetails = ({details, loaded}) => {
         <Fragment>
             <h2>{details.title.label}</h2>
             <img src={details["im:image"][2].label}></img>
-            <audio controls>
+            <audio controls ref={audioRef}>
                 <source src={details.link[1].attributes.href} type={details.link[1].attributes.type}></source>
             </audio>
         </Fragment>
