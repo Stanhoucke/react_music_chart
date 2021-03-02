9b13c1da71523e85b01033bac8d12cc074248243
Stanhoucke <stanhoucke@gmail.com>
2021-03-02 16:51:02 +0000

    api fetch successful, data passed down to SongList component

--- src/containers/ChartBox.js
+++ src/containers/ChartBox.js
@@ -1,12 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import SongDetails from '../components/SongDetails';
 import SongList from '../components/SongList';
 
 const ChartBox = () => {
+
+    const [charts, setCharts] = useState({});
+
+    useEffect(() => {
+        getCharts();
+    },[])
+
+    const getCharts = () => {
+        const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setCharts(data.feed.entry))
+    }
+
     return (
         <Fragment>
             <p>I am a ChartBox</p>
-            <SongList/>
+            <SongList charts={charts}/>
             <SongDetails/>
         </Fragment>
     )
